968914b2bd4c6a6e539398e8a8d6917ef4f4c05c
tanviredu <ornobtanvir.git@gmail.com>
2020-05-01 17:06:02 +0600

    added

---
+++ ACM.BL/ACM.BL/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        ///we add the property
+        // make a customer type
+        
+        public int CustomerId {get;private set;}  // this will be auto generated cant be set by anything outside the class
+        public string FirstName {get;set;}
+        public string Lastname {get;set;}
+        public string EmailAddress {get;set;}
+
+        // make another property
+        public string FullName{
+            get{
+                return Lastname + "," + FirstName;
+            }
+        }
+        
+
+
+    	
+
+    }
+}
